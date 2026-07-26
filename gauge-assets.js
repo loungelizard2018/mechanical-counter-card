03c9c8c32817c2a086f7dd9b6920030119325100
Florian Lechner <43698332+loungelizard2018@users.noreply.github.com>
2026-07-26 10:53:59 +0200

    Add gauge asset module index

---
+++ gauge-assets.js
@@ -0,0 +1,13 @@
+import screwTopLeft from "./gauge-asset-screw-tl.js";
+import screwTopRight from "./gauge-asset-screw-tr.js";
+import screwBottomLeft from "./gauge-asset-screw-bl.js";
+import screwBottomRight from "./gauge-asset-screw-br.js";
+import housingTexture from "./gauge-asset-housing-texture.js";
+
+export const MECHANICAL_COUNTER_GAUGE_ASSETS = Object.freeze({
+  "screw-tl.webp": screwTopLeft,
+  "screw-tr.webp": screwTopRight,
+  "screw-bl.webp": screwBottomLeft,
+  "screw-br.webp": screwBottomRight,
+  "housing-texture.webp": housingTexture,
+});
