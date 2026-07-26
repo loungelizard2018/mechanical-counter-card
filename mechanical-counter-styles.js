827e83bffb92a8980f852abb4f3145f728b2cd9a
Florian Lechner <43698332+loungelizard2018@users.noreply.github.com>
2026-07-26 13:22:45 +0200

    Cache-bust style fragments for v1.4.3

--- mechanical-counter-styles.js
+++ mechanical-counter-styles.js
@@ -1,5 +1,5 @@
-import part1 from "./mechanical-counter-styles-1.js";
-import part2 from "./mechanical-counter-styles-2.js";
-import part3 from "./mechanical-counter-styles-3.js";
+import part1 from "./mechanical-counter-styles-1.js?v=1.4.3";
+import part2 from "./mechanical-counter-styles-2.js?v=1.4.3";
+import part3 from "./mechanical-counter-styles-3.js?v=1.4.3";
 
 export const MECHANICAL_COUNTER_STYLES = `${part1}${part2}${part3}`;
