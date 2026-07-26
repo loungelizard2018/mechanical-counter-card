7a4141d1a5c0e8d127ed39257d18fedfe90fcbc5
Florian Lechner <43698332+loungelizard2018@users.noreply.github.com>
2026-07-26 13:22:56 +0200

    Remove obsolete bitmap screw asset and cache-bust housing texture

--- gauge-assets.js
+++ gauge-assets.js
@@ -1,7 +1,5 @@
-import screwMaster from "./gauge-asset-screw-tr.js";
-import housingTexture from "./gauge-asset-housing-texture.js";
+import housingTexture from "./gauge-asset-housing-texture.js?v=1.4.3";
 
 export const MECHANICAL_COUNTER_GAUGE_ASSETS = Object.freeze({
-  "screw-master.webp": screwMaster,
   "housing-texture.webp": housingTexture,
 });
