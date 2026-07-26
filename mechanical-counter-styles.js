1334ee99a67715fa73c3a210397f2689462dbf85
Florian Lechner <43698332+loungelizard2018@users.noreply.github.com>
2026-07-26 10:58:00 +0200

    Add card style module index

---
+++ mechanical-counter-styles.js
@@ -0,0 +1,5 @@
+import part1 from "./mechanical-counter-styles-1.js";
+import part2 from "./mechanical-counter-styles-2.js";
+import part3 from "./mechanical-counter-styles-3.js";
+
+export const MECHANICAL_COUNTER_STYLES = `${part1}${part2}${part3}`;
