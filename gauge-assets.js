3f96806c670bfca8becb4900d296441270b6c9fd
Florian Lechner <43698332+loungelizard2018@users.noreply.github.com>
2026-07-26 12:08:20 +0200

    Use one verified clean gauge screw asset

--- gauge-assets.js
+++ gauge-assets.js
@@ -1,13 +1,7 @@
-import screwTopLeft from "./gauge-asset-screw-tl.js";
-import screwTopRight from "./gauge-asset-screw-tr.js";
-import screwBottomLeft from "./gauge-asset-screw-bl.js";
-import screwBottomRight from "./gauge-asset-screw-br.js";
+import screwMaster from "./gauge-asset-screw-tr.js";
 import housingTexture from "./gauge-asset-housing-texture.js";
 
 export const MECHANICAL_COUNTER_GAUGE_ASSETS = Object.freeze({
-  "screw-tl.webp": screwTopLeft,
-  "screw-tr.webp": screwTopRight,
-  "screw-bl.webp": screwBottomLeft,
-  "screw-br.webp": screwBottomRight,
+  "screw-master.webp": screwMaster,
   "housing-texture.webp": housingTexture,
 });
